79b1ee59d0d91c1e595c03b9c1b6e79e2474a05f
Arseniy Terehow <arseni_90@mail.ru>
2017-04-07 09:42:22 +0300

    123

--- Hometask_4.2/src/ru/spbu/terekhow/tests/TestHashTable.java
+++ Hometask_4.2/src/ru/spbu/terekhow/tests/TestHashTable.java
@@ -12,15 +12,17 @@ import java.util.ArrayList;
 public class TestHashTable {
 
     @Test
-    public void test() {
+    public void testHashTable1() {
+        final int MAXSTRINGLENGTH = 200;
+
         HashTable<String, Integer> table = new HashTable<>(100000);
-        table.setHash(new StringEasyHash());
+        table.setHash(new StringPowHash());
 
         ArrayList<String> tableItems = new ArrayList<>();
         for (int i = 0; i < 100000; i++) {
-            String s = Integer.toString((int)(Math.random() * 10000000000.0));
+            String s = generateRandomString(MAXSTRINGLENGTH);
             while (table.getValue(s) != null) {
-                s = Integer.toString((int)(Math.random() * 10000000000.0));
+                s = generateRandomString(MAXSTRINGLENGTH);
             }
             table.insert(s, i);
             tableItems.add(s);
@@ -30,8 +32,68 @@ public class TestHashTable {
             assertEquals(i, (int) table.getValue(tableItems.get(i)));
         }
 
-        System.out.println(table.getMaxSizeOfList());
-        System.out.println(table.getAverageSizeOfList());
+        System.out.println("Max size of list: " + table.getMaxSizeOfList());
+        System.out.println("Average size of list: " + table.getAverageSizeOfList());
         assertEquals(100000, table.getCountOfItems());
+    }
+
+    @Test
+    public void testHashTable2() {
+        HashTable<String, Integer> table = new HashTable<>(100000);
+        table.setHash(new StringPowHash());
+
+        ArrayList<String> tableItems = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            String s = Integer.toString((int) (Math.random() * 1000000000));
+            while (table.getValue(s) != null) {
+                s = Integer.toString((int) (Math.random() * 1000000000));
+            }
+            table.insert(s, i);
+            tableItems.add(s);
+        }
+
+        for (int i = 0; i < 100000; i++) {
+            assertEquals(i, (int) table.getValue(tableItems.get(i)));
+        }
+
+        System.out.println("Max size of list: " + table.getMaxSizeOfList());
+        System.out.println("Averqage size of list: " + table.getAverageSizeOfList());
+        assertEquals(100000, table.getCountOfItems());
+    }
+
+    @Test
+    public void testCountOfCollision() {
+        HashTable<Integer, Integer> table = new HashTable<>(10);
+        table.insert(1, 1);
+        table.insert(11, 2);
+        table.insert(111, 3);
+        table.insert(1111, 4);
+        assertEquals(1, table.getCountOfCollision());
+        table.insert(2, 1);
+        table.insert(22, 2);
+        assertEquals(2, table.getCountOfCollision());
+        table.delete(1);
+        table.delete(11);
+        table.delete(111);
+        assertEquals(1, table.getCountOfCollision());
+        table.delete(22);
+        assertEquals(0, table.getCountOfCollision());
+        table.insert(3, 1);
+        assertEquals(0, table.getCountOfCollision());
+        table.insert(3, 1);
+        assertEquals(0, table.getCountOfCollision());
+        table.insert(3, 2);
+        assertEquals(0, table.getCountOfCollision());
+        table.insert(13, 1);
+        assertEquals(1, table.getCountOfCollision());
+    }
+
+    public String generateRandomString(int maxsize) {
+        int size = (int) (1 + Math.random() * maxsize);
+        String str = "";
+        for (int i = 0; i < size; i++) {
+            str += (char) 'a' + (int) (Math.random() * 26);
+        }
+        return str;
     }
 }
